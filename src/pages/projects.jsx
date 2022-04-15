f854165c6989bd90e95428f8e3dddb71537c2ea7
Ayako Woollan <ayako.woollan@gmail.com>
2022-04-15 23:14:49 +1000

    adding layouts

---
+++ src/pages/projects.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from '../layouts/Card'
+import FlexContainer from '../components/flex/FlexContainer'
+
+export default function Projects() {
+    return (
+        <div>
+
+            <FlexContainer>
+                <Card>
+                </Card>
+            </FlexContainer>
+
+        </div>
+    )
+}
