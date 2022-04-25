498aa69b47b4b5ecff617e77bce06dce74ddace1
Ayako Woollan <ayako.woollan@gmail.com>
2022-04-25 15:08:54 +1000

    updating

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
 
