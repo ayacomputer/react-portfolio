207229fcf16ce27a65ec73b6665f120932c3432d
Ayako Woollan <ayako.woollan@gmail.com>
2022-05-29 10:50:59 +1000

    Updates

---
+++ precache-manifest.8f4329d6cb4e5608279b5cc923b487cf.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "a50f45469417e3f86b4d",
+    "url": "/react-portfolio/static/css/main.5db1ad8d.chunk.css"
+  },
+  {
+    "revision": "a50f45469417e3f86b4d",
+    "url": "/react-portfolio/static/js/main.a50f4546.chunk.js"
+  },
+  {
+    "revision": "1b7902a9c385e0d99c81",
+    "url": "/react-portfolio/static/js/1.1b7902a9.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "d86eb18fcf86e368a509cb9f9bc5613e",
+    "url": "/react-portfolio/static/media/logo.d86eb18f.png"
+  },
+  {
+    "revision": "3c1a1e21751e6003af073606c0c4d0c8",
+    "url": "/react-portfolio/static/media/paws.3c1a1e21.png"
+  },
+  {
+    "revision": "47b8cfb453b6ae030c0f9c11f05b9962",
+    "url": "/react-portfolio/static/media/event-finder.47b8cfb4.png"
+  },
+  {
+    "revision": "c756bf84313593cf11f8d378169e0d8c",
+    "url": "/react-portfolio/static/media/mvcBlog.c756bf84.png"
+  },
+  {
+    "revision": "172defc63320385e31173a1f7d17506c",
+    "url": "/react-portfolio/static/media/noteTaker.172defc6.png"
+  },
+  {
+    "revision": "d33b024dc8ef67ec733ff779d012f695",
+    "url": "/react-portfolio/static/media/readme.d33b024d.gif"
+  },
+  {
+    "revision": "29f3c63cd5ed08be9d0e78750a9ebcde",
+    "url": "/react-portfolio/static/media/employeeTracker.29f3c63c.png"
+  },
+  {
+    "revision": "54c953b32c6c36a448c0a89f6a1054fc",
+    "url": "/react-portfolio/static/media/weatherDashboard.54c953b3.png"
+  },
+  {
+    "revision": "5ce79c74724f45b6c360f89eb9f299d2",
+    "url": "/react-portfolio/static/media/CodeQuiz.5ce79c74.gif"
+  },
+  {
+    "revision": "6ff46beaa974aae0e8bfd5e154125c60",
+    "url": "/react-portfolio/static/media/dayPlanner.6ff46bea.gif"
+  },
+  {
+    "revision": "6f0fd487f49f1d4b07cabab890f0783f",
+    "url": "/react-portfolio/static/media/PG.6f0fd487.png"
+  },
+  {
+    "revision": "515017ce65690c26d37d7ae86fcd136f",
+    "url": "/react-portfolio/static/media/jate.515017ce.png"
+  },
+  {
+    "revision": "0cd1072df778653e0a2f6d96f4af5c39",
+    "url": "/react-portfolio/static/media/socialNetworkAPI.0cd1072d.png"
+  },
+  {
+    "revision": "e3d5883f641f6a2ce0b2ea360dbe6e5a",
+    "url": "/react-portfolio/static/media/portfolio.e3d5883f.gif"
+  },
+  {
+    "revision": "d91847b25d58c9fa40c4d88b7aae93af",
+    "url": "/react-portfolio/static/media/cook-helper.d91847b2.png"
+  },
+  {
+    "revision": "e97cbe65fc18c0b08419e2fdb69dadec",
+    "url": "/react-portfolio/index.html"
+  }
+];
